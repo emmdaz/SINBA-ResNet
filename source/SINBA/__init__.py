bf9c4b1bdd7ca3ab7ee46dbf357007f9cf449770
emmdaz <emmanuel.cruzd@alumno.buap.mx>
2026-04-24 10:20:43 -0600

    Correcciones en archivo init.

--- source/SINBA/__init__.py
+++ source/SINBA/__init__.py
@@ -1,1 +1,9 @@
 # __init__
+from .variables_calculator import inv_m, pseudorapidity_separation, trans_m, trans_momentum, leading_n_subleading_jets, met, PRI_jet_num, PRI_jet_all_pt, DER_prodeta_jet_jet, DER_deltaeta_jet_jet
+from .dataset_creator import dataset
+from .residual_block import residual_block
+from .experiment import objective, objective_tracked
+
+__all__ = ["inv_m", "pseudorapidity_separation", "trans_m", "trans_momentum", "leading_n_subleading_jets",
+            "met", "PRI_jet_num", "PRI_jet_all_pt", "DER_prodeta_jet_jet", "DER_deltaeta_jet_jet", "dataset",
+            "residual_block", "objective", "objective_tracked"]
